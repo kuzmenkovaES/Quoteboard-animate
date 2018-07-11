29065b225c0e7943cd39a2404f0eaada1256dd5a
Kate <kuzmenkovaes@gmail.com>
2018-07-11 14:12:06 +0300

    simple changes

--- main.js
+++ main.js
@@ -53,7 +53,7 @@ module.service('mainFactory', [ '$interval', function($interval) {
     };
 
     function formatNumber(number) {
-        return number.toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+        return number.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
     }
 
     function generateDataForNewRow(symbolName){
